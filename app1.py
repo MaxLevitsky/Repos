932879161031f1c37f2367ed3cd025dcd064f081
Lyova <lyova5@gmail.com>
2021-11-08 13:59:07 +0300

    Commit

--- app1.py
+++ app1.py
@@ -1,8 +1,15 @@
 from flask import Flask
+1
+2
+3
+4
+5
+14
 
 app= Flask(__name__)
 
 @app.route("/hello/<name>")
 def hello_world(name):
-    return "<p>Hello, {name}</p>"
+    return "<p>Hello?{name}</p>"
+
 app.run()
